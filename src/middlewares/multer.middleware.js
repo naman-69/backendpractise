371574bc1ad32967f24d15187da8f53aa7766b08
Naman Bansal <namanbansal969@gmail.com>
2024-12-23 01:29:34 +0530

    watch history functionality added

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -47,9 +47,10 @@ import multer from "multer";
 //   { name: 'resume', maxCount: 1 },
 // ]);
 
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './assets/temp');
+    cb(null, './src/assets/temp');
   },
   filename: function (req, file, cb) {
     cb(null, `${Date.now()}-${file.originalname}`);
